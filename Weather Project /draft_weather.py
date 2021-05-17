c67f4946c3286b7409bbc15555cab2bf689e3ef5
Alexia Diaz <alexiadiaz@Alexias-MBP-2.attlocal.net>
2021-05-16 19:44:46 -0500

    Added exception handling

--- Weather Project /draft_weather.py
+++ Weather Project /draft_weather.py
@@ -4,12 +4,20 @@ import json
 import weather_service
 
 # Create a Python Application which asks the user for their zip code or city.
-zip_code = input("Enter zip code: ")
-city_name = input("Enter City Name: ")
+while True:
+    zip_code = input("Enter zip code: ")
+    city_name = input("Enter City Name: ")
 
-if zip_code != '':
-    weather_json = weather_service.get_weather_by_zip(zip_code)
-    print(json.dumps(weather_json, indent=4, sort_keys=True))
-elif city_name != '':
-    weather_json = weather_service.get_weather_by_city(city_name)
-    print(json.dumps(weather_json, indent=4, sort_keys=True))
+    if zip_code != '' and zip_code.isnumeric():
+        weather_json = weather_service.get_weather_by_zip(zip_code)
+        print(json.dumps(weather_json, indent=4, sort_keys=True))
+    elif city_name != '' and city_name.isalpha():
+        weather_json = weather_service.get_weather_by_city(city_name)
+        print(json.dumps(weather_json, indent=4, sort_keys=True))
+    else:
+        print("The data you supplied is invalid")
+        continue
+
+    exit_app = input("\nWould you like to exit application (Yes or No): ").upper()
+    if exit_app == 'YES':
+        break
--- Weather Project /weather_service.py
+++ Weather Project /weather_service.py
@@ -3,15 +3,32 @@ import requests
 api_key = 'c8c72ade501e8dc2ae754aced82bb3ad'
 zip_api_uri = 'https://api.openweathermap.org/data/2.5/weather?zip={},us&appid={}'
 city_name_api_uri = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid={}'
+timeout_exception = 'A timeout has occurred while fetching data'
+connection_exception = 'A error has occurred while attempting to make a connection to web service'
 
 
 # Get weather by zip
 def get_weather_by_zip(zipcode):
-    uri = zip_api_uri.format(zipcode, api_key)
-    return requests.get(uri).json()
+    try:
+        uri = zip_api_uri.format(zipcode, api_key)
+        result = requests.get(uri).json()
+
+    except requests.exceptions.Timeout:
+        result = timeout_exception
+    except requests.exceptions.ConnectionError:
+        result = connection_exception
+
+    return result
 
 
 # Get weather by city
 def get_weather_by_city(city):
-    uri = city_name_api_uri.format(city, api_key)
-    return requests.get(uri).json()
+    try:
+        uri = city_name_api_uri.format(city, api_key)
+        result = requests.get(uri).json()
+    except requests.exceptions.Timeout:
+        result = timeout_exception
+    except requests.exceptions.ConnectionError:
+        result = connection_exception
+
+    return result
